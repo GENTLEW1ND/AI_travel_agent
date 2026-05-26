6c08ad34d8904887b13e5a9d91375dadfc1f5552
GENTLEW1ND <rajchakraborty029@gmail.com>
2026-05-26 16:21:13 +0000

    Add all the required agents

--- main.py
+++ main.py
@@ -50,3 +50,75 @@ def flight_agent(state: TravelState):
         ],
         "llm_calls": state.get("llm_calls", 0) + 1
     }
+    
+def hotel_agent(state: TravelState):
+    query = f"Best hostels for {state["user_query"]}"
+    hotel_result = tavily_search(query)
+    
+    return{
+        "hotel_results" : hotel_result,
+        "message": [
+            AIMessage(content = f"Hotel information fetched")
+        ],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
+    
+def itinerary_agent(state: TravelState):
+    prompt = f"""
+        Create a travel itinerary,
+        User Query:
+        {state['user_query']}
+        
+        Flight Results:
+        {state['flight_results']}
+        
+        Hotel Results:
+        {state['hotel_results']}
+    """
+    response = llm.invoke({
+        SystemMessage(
+            content = "You are an expernt travel planner"
+        ),
+        HumanMessage(content=prompt)
+    })
+    
+    return{
+        "Itinerary" : response.content,
+        "message": [response],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
+    
+def final_agent(state: TravelState):
+    final_prompt = f"""
+        Generate final travel response,
+        Flights:
+        {state['flight_results']}
+        
+        Hotels:
+        {state['hotel_results']}
+        
+        Itinerary:
+        {state['itinerary']}
+    """
+    response = llm.invoke({
+        HumanMessage(content=final_prompt)
+    })
+    
+    return{
+        "message": [response],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
+    
+    
+graph = StateGraph(TravelState)
+
+graph.add_node("flight_agent", flight_agent)
+graph.add_node("hotel_agent", hotel_agent)
+graph.add_node("itinenary_agent", itinerary_agent)
+graph.add_node("final_agent", final_agent)
+
+graph.add_edge(START, "flight_agent")
+graph.add_edge("flight_agent", "hotel_agent")
+graph.add_edge("hotel_agent", "itinerary_agent")
+graph.add_edge("itinerary_agent", "final_agent")
+graph.add_edge("final_agent", END)
